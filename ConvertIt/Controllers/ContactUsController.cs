990c24e44f790803ab55765685300325e960e8b9
Gittie Klein <gittiekay@gmail.ciom>
2018-06-18 22:05:42 -0400

    GK - add email

---
+++ ConvertIt/Controllers/ContactUsController.cs
@@ -0,0 +1,25 @@
+﻿using ConvertIt.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConvertIt.Controllers
+{
+    public class ContactUsController : Controller
+    {
+        EmailService EmailService = new EmailService();
+
+        // GET: ContactUs
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public void SendEmail()
+        {
+            EmailService.SendEmail("gittiekay@gmail.com", "testemail", "dear blank<br>This is a test email lol<br>hope it works");
+        }
+    }
+}
---
+++ ConvertIt/Services/EmailService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+
+namespace ConvertIt.Services
+{
+    public class EmailService
+    {
+        readonly string emailFrom = "convertitconversions@gmail.com";
+        readonly string fromPassword = "neumannklein";
+
+        public void SendEmail(string emailTo, string subject, string body)
+        {                 
+            var fromAddress = new MailAddress(emailFrom, "ConvertIt");
+            var toAddress = new MailAddress(emailTo);
+
+            AlternateView alternateView = CreateEmailBody(body);         
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body,
+                AlternateViews = { alternateView }
+            })
+            {
+                smtp.Send(message);
+            }
+        }
+
+        public AlternateView CreateEmailBody(string body)
+        {
+            var linkedResource = new LinkedResource("C:\\Users\\Gittie Klein\\OneDrive\\Documents\\Projects\\ConvertIt\\ConvertIt\\Images\\logo.jpg", MediaTypeNames.Image.Jpeg);
+
+            var htmlBody = $"<p>{body}</p><br><p>The ConvertIt Team</p><br><img style='width:230px' src=\"cid:{linkedResource.ContentId}\"/>";
+
+            var alternateView = AlternateView.CreateAlternateViewFromString(htmlBody, null, MediaTypeNames.Text.Html);
+            alternateView.LinkedResources.Add(linkedResource);
+
+            return alternateView;
+        }
+
+
+
+    }
+}
